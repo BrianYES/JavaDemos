0d9fea41be2772f3a24d7e26ad56d7ab17321f8e
BrianYES <xyo824@163.com>
2018-05-10 16:11:25 +0800

    activemq queue方式

---
+++ activemq/src/test/java/com/brian/test/ActiveMQTest.java
@@ -0,0 +1,73 @@
+package com.brian.test;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
+
+public class ActiveMQTest {
+
+    @Test
+    public void testQueueProducer() throws JMSException {
+        // 创建一个连接工厂对象ConnectionFactory对象。需要制动mq服务的ip和端口
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        // 使用ConnectionFactory对象创建一个Connection对象
+        Connection connection = connectionFactory.createConnection();
+        // 开启连接。调用Connection对象的start方法
+        connection.start();
+        // 使用Connection对象创建一个Session对象
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 使用Session对象创建一个Destination对象，两种形式queue和topic
+        Queue queue = session.createQueue("test-queue");
+        // 使用Session对象创建一个Producer对象
+        MessageProducer producer = session.createProducer(queue);
+        // 创建一个TestMessage对象
+        TextMessage textMessage = session.createTextMessage("hello activemq");
+        // 发送消息
+        producer.send(textMessage);
+        // 关闭资源
+        producer.close();
+        session.close();
+        connection.close();
+    }
+
+    @Test
+    public void testQueueConsumer() throws Exception {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Queue queue = session.createQueue("test-queue");
+        MessageConsumer consumer = session.createConsumer(queue);
+        consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        String text = textMessage.getText();
+                        System.out.println(text);
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        // 系统等到接受消息
+        System.in.read();
+
+        // 关闭资源
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+}
